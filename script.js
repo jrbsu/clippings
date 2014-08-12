0178e82f4ef8d4eb48509d5c5ef6183e0eb01d53
jrbsu <hello@jossu.co.uk>
2014-08-13 00:20:05 +0100

    Tweaks

--- script.js
+++ script.js
@@ -8,24 +8,40 @@ function replaceStuff(){
 	var clippings = "";
 	var urls = "";
 	var works = "";
-	workLength = 0;
+	var workLength = 0;
+	var clippingLength;
 		
-	var findWholeClipping = /(.*\s-\s.*\nhttp.*|January.*|February.*|March.*|April.*|May.*|June.*|July.*|August.*|September.*|October.*|November.*|December.*)/g //Finds a whole clipping
-	var findClipping = /(.*\s-\s.*|January.*|February.*|March.*|April.*|May.*|June.*|July.*|August.*|September.*|October.*|November.*|December.*)/ //Finds the work-title thing (or the date)
-	var findURL = /http.*/g //Finds the URL
-	var findWork = /(.*?)\s-\s/ //Finds the hyphen separator
-		
-	var wholes = text.match(findWholeClipping); //put clippings in array
+	// Finds a whole clipping
+	var findWholeClipping = /(.*\s-\s.*\nhttp.*|January.*|February.*|March.*|April.*|May.*|June.*|July.*|August.*|September.*|October.*|November.*|December.*)/g;
+	
+	// Finds the work-title thing (or the date)
+	var findClipping = /(.*\s-\s.*|January.*|February.*|March.*|April.*|May.*|June.*|July.*|August.*|September.*|October.*|November.*|December.*)/;
+	
+	// Finds the URL
+	var findURL = /http.*/g;
+  
+	// Finds the hyphen separator
+	var findWork = /(.*?)\s-\s/;
+	
+	// put clippings in array
+	var wholes = text.match(findWholeClipping);
 	console.log(wholes); //debugging yay
-	for (var i=0; i<wholes.length; i++) {
-		clippings = wholes[i].match(findClipping); //put clipping details in array
-		urls = wholes[i].match(findURL); //put url in array
-		works = wholes[i].match(findWork); //put work in array
-		clippingLength = clippings[0].length; // To catch the clipping length
-		if (works != null){ //If this is null it means it's found a date, not a clipping...
-			workLength = works[0].length; // To catch the work length
-			x = "*" + works[0] + "[" + urls[0] + " " + clippings[0].substring(workLength,clippingLength) + "]\n";
-		} else { //...So it should just paste the date in verbatim
+	for (var i = 0; i < wholes.length; i++) {
+		// put clipping details in array
+		clippings = wholes[i].match(findClipping); 
+		// put url in array
+		urls = wholes[i].match(findURL);
+		// put work in array
+		works = wholes[i].match(findWork);
+		// To catch the clipping length
+		clippingLength = clippings[0].length; 
+		// If this is null it means it's found a date, not a clipping...
+		if (works !== null) {
+  			// To catch the work length
+			workLength = works[0].length; 
+			x = "*" + works[0] + "[" + urls[0] + " " + clippings[0].substring(workLength, clippingLength) + "]\n";
+		} else { 
+  			// ...So it should just paste the date in verbatim
 			workLength = 0;
 			x = "\n;" + clippings[0] + "\n";
 		}
